8e33dd9f8c0f0b58a4bfc3cc06cdc5d8baa5c17a
咸泰裕 <kanyasuhiro@minayasironoMBP.lan>
2024-07-29 21:58:11 +0900

    chang img of term

--- resources/views/campaign/term.blade.php
+++ resources/views/campaign/term.blade.php
@@ -37,68 +37,100 @@
     </div>
   </section>
   <x-campaign>
-    <x-slot name="campaign_name">デトックスCampaign</x-slot>
-    <x-slot name="menu_img">../img/course28.jpg</x-slot>
-    <x-slot name="menu_alt">美容鍼灸の施術写真</x-slot>
-    <x-slot name="menu_name">美容鍼灸&nbsp;90分</x-slot>
-    <x-slot name="menu_price">14300円→13500円</x-slot>
-    <x-slot name="conditions_text_term">2024年6月1日〜2024年6月30日迄</x-slot>
+    <x-slot name="campaign_name">美容鍼Campaign</x-slot>
+    <x-slot name="menu_img">../img/course240801.jpg</x-slot>
+    <x-slot name="menu_alt">美容鍼の施術写真</x-slot>
+    <x-slot name="menu_name">アンチエイジング美容鍼(顔＋全身)90分</x-slot>
+    <x-slot name="menu_price">90分&nbsp;14300円→13500円</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
   </x-campaign>
   <x-campaign>
-    <x-slot name="campaign_name">デトックスCampaign</x-slot>
-    <x-slot name="menu_img">../img/course33.jpg</x-slot>
-    <x-slot name="menu_alt">鍼治療の施術写真</x-slot>
-    <x-slot name="menu_name">美容鍼灸60分+経路デトックス鍼治療60分</x-slot>
+    <x-slot name="campaign_name">美容鍼Campaign</x-slot>
+    <x-slot name="menu_img">../img/course240802.jpg</x-slot>
+    <x-slot name="menu_alt">美容鍼の施術写真</x-slot>
+    <x-slot name="menu_name">アンチエイジング美容鍼60分+自律神経鍼60分</x-slot>
     <x-slot name="menu_price">120分&nbsp;18200円→17500円</x-slot>
-    <x-slot name="conditions_text_term">2024年6月1日〜2024年6月30日迄</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
   </x-campaign>
   <x-campaign>
-    <x-slot name="campaign_name">デトックスCampaign</x-slot>
-    <x-slot name="menu_img">../img/course34.jpg</x-slot>
-    <x-slot name="menu_alt">美容鍼灸の施術写真</x-slot>
-    <x-slot name="menu_name">ミネラル海藻マスク美容鍼灸90分</x-slot>
-    <x-slot name="menu_price">90分&nbsp;15000円→13500円</x-slot>
-    <x-slot name="conditions_text_term">2024年6月1日〜2024年6月30日迄</x-slot>
+    <x-slot name="campaign_name">美容鍼Campaign</x-slot>
+    <x-slot name="menu_img">../img/course240803.jpg</x-slot>
+    <x-slot name="menu_alt">美容鍼の施術写真</x-slot>
+    <x-slot name="menu_name">アンチエイジング美容鍼60分+全身むくみリンパオイル60分</x-slot>
+    <x-slot name="menu_price">120分&nbsp;18200円→17500円</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
   </x-campaign>
   <x-campaign>
-    <x-slot name="campaign_name">デトックスCampaign</x-slot>
-    <x-slot name="menu_img">../img/course36.jpg</x-slot>
-    <x-slot name="menu_alt">マスクフェイシャルの施術写真</x-slot>
-    <x-slot name="menu_name">ミネラル海藻マスクフェイシャル90分</x-slot>
-    <x-slot name="menu_price">90分&nbsp;11000円→9800円</x-slot>
-    <x-slot name="conditions_text_term">2024年6月1日〜2024年6月30日迄</x-slot>
+    <x-slot name="campaign_name">美容鍼Campaign</x-slot>
+    <x-slot name="menu_img">../img/course240804.jpg</x-slot>
+    <x-slot name="menu_alt">美容鍼の施術写真</x-slot>
+    <x-slot name="menu_name">自律神経鍼+リンパオイル90分</x-slot>
+    <x-slot name="menu_price">90分&nbsp;12000円→10000円</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
   </x-campaign>
   <x-campaign>
-    <x-slot name="campaign_name">デトックスCampaign</x-slot>
-    <x-slot name="menu_img">../img/course32.jpg</x-slot>
-    <x-slot name="menu_alt">鍼治療の施術写真</x-slot>
-    <x-slot name="menu_name">漢方浴+経路デトックス鍼治療90分</x-slot>
-    <x-slot name="menu_price">90分&nbsp;14300円→12500円</x-slot>
-    <x-slot name="conditions_text_term">2024年6月1日〜2024年6月30日迄</x-slot>
+    <x-slot name="campaign_name">美容鍼Campaign</x-slot>
+    <x-slot name="menu_img">../img/course240805.jpg</x-slot>
+    <x-slot name="menu_alt">美容鍼の施術写真</x-slot>
+    <x-slot name="menu_name">ヘットスパ鍼+自律神経鍼90分</x-slot>
+    <x-slot name="menu_price">90分&nbsp;12000円→10000円</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
   </x-campaign>
   <x-campaign>
-    <x-slot name="campaign_name">デトックスCampaign</x-slot>
-    <x-slot name="menu_img">../img/course31.jpg</x-slot>
+    <x-slot name="campaign_name">美容鍼Campaign</x-slot>
+    <x-slot name="menu_img">../img/course240806.jpg</x-slot>
+    <x-slot name="menu_alt">美容鍼の施術写真</x-slot>
+    <x-slot name="menu_name">美容鍼+カーボキシー高濃度炭酸パック90分</x-slot>
+    <x-slot name="menu_price">90分&nbsp;16500円→15000円</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
+  </x-campaign>
+  <x-campaign>
+    <x-slot name="campaign_name">美容鍼Campaign</x-slot>
+    <x-slot name="menu_img">../img/course240807.jpg</x-slot>
+    <x-slot name="menu_alt">美容鍼の施術写真</x-slot>
+    <x-slot name="menu_name">ハーブピーリング+美容鍼90分</x-slot>
+    <x-slot name="menu_price">90分&nbsp;22000円→19000円</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
+  </x-campaign>
+  <x-campaign>
+    <x-slot name="campaign_name">リラクゼーションCampaign</x-slot>
+    <x-slot name="menu_img">../img/course240808.jpg</x-slot>
+    <x-slot name="menu_alt">フェイシャルの施術写真</x-slot>
+    <x-slot name="menu_name">カーボキシー高濃度炭酸パックフェイシャル80分</x-slot>
+    <x-slot name="menu_price">80分&nbsp;11000円→9900円</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
+  </x-campaign>
+  <x-campaign>
+    <x-slot name="campaign_name">リラクゼーションCampaign</x-slot>
+    <x-slot name="menu_img">../img/course240809.jpg</x-slot>
+    <x-slot name="menu_alt">フェイシャルの施術写真</x-slot>
+    <x-slot name="menu_name">セルキュア小顔リフトフェイシャル80分</x-slot>
+    <x-slot name="menu_price">80分&nbsp;11000円→9900円</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
+  </x-campaign>
+  <x-campaign>
+    <x-slot name="campaign_name">リラクゼーションCampaign</x-slot>
+    <x-slot name="menu_img">../img/course240810.jpg</x-slot>
     <x-slot name="menu_alt">リンパオイルの施術写真</x-slot>
-    <x-slot name="menu_name">自律神経鍼+浮腫リンパオイル90分</x-slot>
-    <x-slot name="menu_price">90分&nbsp;11000円→9900円</x-slot>
-    <x-slot name="conditions_text_term">2024年6月1日〜2024年6月30日迄</x-slot>
+    <x-slot name="menu_name">ヘッドスパ+全身むくみリンパオイル90分</x-slot>
+    <x-slot name="menu_price">90分&nbsp;12000円→10000円</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
   </x-campaign>
   <x-campaign>
-    <x-slot name="campaign_name">デトックスCampaign</x-slot>
-    <x-slot name="menu_img">../img/course30.jpg</x-slot>
-    <x-slot name="menu_alt">リンパデトックスオイルの施術写真</x-slot>
-    <x-slot name="menu_name">浮腫リンパデトックスオイル90分</x-slot>
-    <x-slot name="menu_price">90分&nbsp;10500円→9900円</x-slot>
-    <x-slot name="conditions_text_term">2024年6月1日〜2024年6月30日迄</x-slot>
-  </x-campaign>
-  <x-campaign>
-    <x-slot name="campaign_name">デトックスCampaign</x-slot>
-    <x-slot name="menu_img">../img/course29.jpg</x-slot>
+    <x-slot name="campaign_name">リラクゼーションCampaign</x-slot>
+    <x-slot name="menu_img">../img/course240811.jpg</x-slot>
     <x-slot name="menu_alt">リンパオイルの施術写真</x-slot>
-    <x-slot name="menu_name">海藻ミネラルマスクフェイシャル60分+浮腫リンパオイル60分</x-slot>
-    <x-slot name="menu_price">90分&nbsp;18200円→14200円</x-slot>
-    <x-slot name="conditions_text_term">2024年6月1日〜2024年6月30日迄</x-slot>
+    <x-slot name="menu_name">全身+骨盤ストレッチリンパオイル90分</x-slot>
+    <x-slot name="menu_price">90分&nbsp;11000円→10000円</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
+  </x-campaign>
+  <x-campaign>
+    <x-slot name="campaign_name">リラクゼーションCampaign</x-slot>
+    <x-slot name="menu_img">../img/course240812.jpg</x-slot>
+    <x-slot name="menu_alt">リンパオイルの施術写真</x-slot>
+    <x-slot name="menu_name">カーボキシー高濃度炭酸パックフェイシャル+全身むくみリンパオイル120分</x-slot>
+    <x-slot name="menu_price">120分&nbsp;18200円→15200円</x-slot>
+    <x-slot name="conditions_text_term">2024年8月1日〜2024年9月30日迄</x-slot>
   </x-campaign>
   </main>
   <x-menu.footer></x-menu.footer>
